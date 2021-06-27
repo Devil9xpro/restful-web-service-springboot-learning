3af5ef802c6523c5ddc16a03c2f17ed9b06eaf0f
Devil9xpro <chinhzz3107@gmail.com>
2021-06-27 14:41:13 +0700

    add path variable to api hello world

--- src/main/java/com/example/restfulwebservicespringbootlearning/controller/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservicespringbootlearning/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.example.restfulwebservicespringbootlearning.controller;
 import com.example.restfulwebservicespringbootlearning.entity.HelloWorldBean;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,8 @@ public class HelloWorldController {
         return new HelloWorldBean("Hello World");
     }
 
-    
+    @GetMapping(path = "/hello-world-bean/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
 }
