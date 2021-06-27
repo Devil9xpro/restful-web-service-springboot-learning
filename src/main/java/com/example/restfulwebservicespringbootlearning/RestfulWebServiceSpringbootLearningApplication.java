e61c512fbdb40753859a47214e4d596246d25766
Devil9xpro <chinhzz3107@gmail.com>
2021-06-27 14:09:12 +0700

    first commit

---
+++ src/main/java/com/example/restfulwebservicespringbootlearning/RestfulWebServiceSpringbootLearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservicespringbootlearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceSpringbootLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServiceSpringbootLearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restfulwebservicespringbootlearning/RestfulWebServiceSpringbootLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservicespringbootlearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceSpringbootLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
