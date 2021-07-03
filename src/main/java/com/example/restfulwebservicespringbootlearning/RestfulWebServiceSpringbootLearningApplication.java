6ae6c72d7b87652e39ed0037074594ae3f4ab158
Devil9xpro <chinhzz3107@gmail.com>
2021-07-03 14:53:30 +0700

    add internationalization

--- src/main/java/com/example/restfulwebservicespringbootlearning/RestfulWebServiceSpringbootLearningApplication.java
+++ src/main/java/com/example/restfulwebservicespringbootlearning/RestfulWebServiceSpringbootLearningApplication.java
@@ -1,7 +1,14 @@
 package com.example.restfulwebservicespringbootlearning;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServiceSpringbootLearningApplication {
@@ -10,4 +17,24 @@ public class RestfulWebServiceSpringbootLearningApplication {
 		SpringApplication.run(RestfulWebServiceSpringbootLearningApplication.class, args);
 	}
 
+	// @Bean
+	// public LocaleResolver localeResolver() {
+	// 	SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+	// 	localeResolver.setDefaultLocale(Locale.US);
+	// 	return localeResolver;
+	// }
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
 }
--- src/main/java/com/example/restfulwebservicespringbootlearning/controller/HelloWorldController.java
+++ src/main/java/com/example/restfulwebservicespringbootlearning/controller/HelloWorldController.java
@@ -1,15 +1,23 @@
 package com.example.restfulwebservicespringbootlearning.controller;
 
+import java.util.Locale;
+
 import com.example.restfulwebservicespringbootlearning.entity.HelloWorldBean;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -25,4 +33,16 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
     }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(
+            @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, locale);
+    }
+
+    @GetMapping(path = "/hello-world-internationalized2")
+    public String helloWorldInternationalized2() {
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
+    }
+
 }
